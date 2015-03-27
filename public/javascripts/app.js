d6dd03bc72c6754092a7ccd1406dfd07a0388663
jovialgent <gfrankpetersen@gmail.com>
2015-03-26 18:47:58 -0600

    Testing Angular with Require.JS

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,23 +1,21 @@
-//Filename: boilerplate.js
+(function() {
+    define([
+        'jquery',
+        'underscore',
+        'alertify',
+        'angular',
+        'test'
+    ], function($, _, Alertify, angular, Test) {
+        var initialize = function() {
+            Test.runTest();
+            angular.bootstrap(document, ['demo']);
 
-define([
-    // These are path alias that we configured in our bootstrap
-    'jquery',     // lib/jquery/jquery
-    'underscore', // lib/underscore/underscore
-    'backbone',
-    'alertify'
-], function($, _, Backbone, Alertify){
-    
-    var initialize = function(){
-       Alertify.message("WORKS?");
+        }
 
-    }
-    
-    // Above we have passed in jQuery, Underscore and Backbone
-    // They will not be accessible in the global scope
-    return {
-        initialize:initialize
-        
-    };
-    // What we return here will be used by other modules
-});
+        return {
+            initialize: initialize
+
+        };
+    });
+
+})()
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,27 +1,31 @@
-// Require.js allows us to configure shortcut alias
-// There usage will become more apparent further along in the tutorial.
-require.config({
-	baseUrl: "javascripts",
-    paths: {
-        jquery: 'libs/jquery/jquery-2.1.1',
-        underscore: 'libs/underscore/underscore',
-        backbone: 'libs/backbone/backbone',
-        cytoscape : "libs/cytoscape/build/cytoscape",
-        alertify : "libs/alertify/alertify.min"
-    },
-    shim : {
-        "cytoscape" : {
-            deps : ["jquery"]
+(function() {
+    require.config({
+        baseUrl: "javascripts",
+        paths: {
+            jquery: 'libs/jquery/jquery-2.1.1',
+            underscore: 'libs/underscore/underscore',
+            angular: 'libs/angular/angular.min',
+            cytoscape: "libs/cytoscape/build/cytoscape",
+            alertify: "libs/alertify/alertify.min"
+        },
+        shim: {
+            "cytoscape": {
+                deps: ["jquery"]
+            },
+            'angular': {
+                exports: 'angular'
+            }
+
+
         }
-    }
 
-});
+    });
 
-require([
+    require([
+        'app',
+    ], function(App) {
+        App.initialize();
 
-    // Load our app module and pass it to our definition function
-    'app',
-], function(App){
-    // The "app" dependency is passed in as "App"
-    App.initialize();
-});
+    });
+
+})()
